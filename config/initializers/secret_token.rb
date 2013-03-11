04c862d83bcbc4b8e5a4d60deec4ea011d14ae5a
Alec Matias <alec.matias@gmail.com>
2013-03-11 16:12:28 -0700

    Initial generation of Grocer rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grocr::Application.config.secret_token = '9cddfca047dc19e7abbee02d4d033e9e8fcf8bb74166499acbff127c8859718bc19666d5aa939701c9291adef95239b76571dff80565d499bab2571ef4c1e778'
