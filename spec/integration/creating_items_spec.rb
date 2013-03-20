e4a5facc0abf86b39f3abb955c34769fc72a5b2a
alphang <alphang@server.fake>
2013-03-20 11:53:36 -0700

    Cleanup. Deleting temporary files.

--- spec/integration/creating_items_spec.rb
+++ spec/integration/creating_items_spec.rb
@@ -9,4 +9,13 @@ feature "Creating Items" do
     click_button 'Create Item'
     page.should have_content('Item 123 has been added.')
   end
+
+  scenario "cannot create an item with bad params" do
+    visit '/'
+    click_link "Add an Item"
+    fill_in 'Product ID', :with => 123
+    fill_in 'Quantity', :with =>2
+    click_button 'Create Item'
+    page.should_not
+  end
 end
