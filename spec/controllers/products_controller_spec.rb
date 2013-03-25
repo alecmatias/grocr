ba6d135617b16d1c02c41c25492c5abe5ab3a7ba
alphang <alphang@server.fake>
2013-03-25 11:22:16 -0700

    Rename products_spec to products_controller_spec

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+describe ProductsController do
+  it "displays an error for a missing product" do
+    get :show, :id => "not-here"
+    response.should redirect_to(product_path)
+    message = "The product you were looking for could not be found."
+    flash[:alert].should == message
+  end
+end
