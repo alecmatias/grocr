a6d8c63b822af62aa10b323b0575a1a79b1faf02
Mike Begley <michael.l.begley@gmail.com>
2013-03-25 13:39:31 -0700

    fix bug in message when a product has been deleted

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 describe ProductsController do
   it "displays an error for a missing product" do
     get :show, :id => "not-here"
-    response.should redirect_to(product_path)
+    response.should redirect_to products_path
     message = "The product you were looking for could not be found."
     flash[:alert].should == message
   end
