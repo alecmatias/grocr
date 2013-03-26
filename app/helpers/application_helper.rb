0d954a07c8c46c6e8bc5cc338d3c0c1c43145cd5
Alec Matias <alec.matias@gmail.com>
2013-03-26 15:05:19 -0700

    Lock down specific products controller actions for admin only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#admin_user = User.create(:email => "admin@example.com", :password => "admin")
-#admin_user.admin = true
+admin_user = User.create(:email => "admin@example.com", :password => "adminpassword")
+admin_user.admin = true
+admin_user.save!
 #admin_user.confirm!
 
 Product.create(
-               :name => "Seeded Product 1", 
-               :manufacturer => "M1", 
+               :name => "Seeded Product 1",
+               :manufacturer => "M1",
                :price => "$3.69",
                :size  => "large",
                :upc => "324234234242"
                )
 
 Product.create(
-               :name => "Seeded Product 2", 
-               :manufacturer => "M2", 
+               :name => "Seeded Product 2",
+               :manufacturer => "M2",
                :price => "5.99",
                :size  => "medium",
                :upc => "78686876876"
                )
 
 Product.create(
-               :name => "Seeded Product 3", 
-               :manufacturer => "M3", 
+               :name => "Seeded Product 3",
+               :manufacturer => "M3",
                :price => "$19.99",
                :size  => "small",
                :upc => "65466546456"
